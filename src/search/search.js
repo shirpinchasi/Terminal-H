00a7f19b34a128665aa55eb6cd03df3b84fac59e
Shir Pinchasi <shirpi9999@gmail.com>
2021-03-31 00:41:44 +0300

    added changes and search bar

--- src/search/search.js
+++ src/search/search.js
@@ -4,43 +4,43 @@ import "./Search.scss";
 import { Link } from "react-router-dom";
 
 function Search() {
-    
+
     const [query, setQuery] = useState("");
     const [product, setProducts] = useState([]);
     const [isLoading, setLoading] = useState(true);
-    const [searchValue, setSearchValue]= useState("");
-    
-    const handleChangeInput = (e) =>{
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleChangeInput = (e) => {
         setSearchValue(e.target.value);
     }
-   const reset = ()=>{
-       setProducts([])
-   }
+    const reset = () => {
+        setProducts([])
+    }
 
-    const CallSearchFunction = (e)=>{
+    const CallSearchFunction = (e) => {
         e.preventDefault();
         reset();
         setQuery(searchValue);
-        
-    }
-    
 
-    useEffect(()=>{
-        
-        if(!query){
+    }
+
+
+    useEffect(() => {
+
+        if (!query) {
             return;
         }
-        
+
         getProducts();
     }, [query]);
 
     async function getProducts() {
-        try{
+        try {
             const res = await fetch(`https://terminal-h.herokuapp.com/api/products/search/findByNameContainingIgnoreCase?name=${query}&projection=detailedProduct`);
             const product = await res.json();
             setProducts(product._embedded.products)
             setLoading(false)
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
         console.log(product);
@@ -48,47 +48,45 @@ function Search() {
     function hasNoResults() {
         return query && product.length === 0;
     }
-   
-    return(
+
+    return (
         <div id="Search">
             <div>
-            <input
-                placeholder="search Product Here.."
-                value={searchValue}
-                onChange={handleChangeInput} 
-                type="text"
+                <input
+                    placeholder="search Product Here.."
+                    value={searchValue}
+                    onChange={handleChangeInput}
+                    type="text"
                 // onKeyUp={dodelaySearch(this.val)}
-            />
-            <input onClick={CallSearchFunction}  type="submit" value="SEARCH"/>
-            
+                />
+                <input onClick={CallSearchFunction} type="submit" value="SEARCH" />
+
             </div>
             <div>
                 {isLoading ? (
-                    <Loading/>
+                    <Loading />
                 ) : (
-
-                
                     <div>
-                    <div className="products">
-                    {product.map(prod=>(
-                        <div>
-                        <Link to={`/ProductPage/${prod.id}`} id="Link">
-                        <div className="ajustProducts">
-                            <img src ={prod.pictureUrl} className="pictureUrlProducts"/>
-                          <div>{prod.name}</div>
-                          <div>{prod.price}</div>
-                          <div className="brandProducts">{prod.brand.name}</div>
+                        <div className="products">
+                            {product.map(prod => (
+                                <div>
+                                    <Link to={`/ProductPage/${prod.id}`} id="Link">
+                                        <div className="ajustProducts">
+                                            <img src={prod.pictureUrl} className="pictureUrlProducts" />
+                                            <div>{prod.name}</div>
+                                            <div>{prod.price}</div>
+                                            <div className="brandProducts">{prod.brand.name}</div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
-                        </Link>
-                        </div>
-                    ))}
+
+
+
                     </div>
-                    
-                
-                
-                  </div>  
-            
-            )}
+
+                )}
             </div>
         </div>
     )
